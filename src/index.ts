db5ab3d487335fa60d5f3d95710294df66c7fb62
ruthierrex <ruthierrex@gmail.com>
2023-10-08 21:58:51 -0300

    added implement repository default

--- src/index.ts
+++ src/index.ts
@@ -1,47 +1,23 @@
 import { Categoria } from "@modules/catalogo/domain/categoria/categoria.entity";
+import { CategoriaPrismaRepository } from "@modules/catalogo/infra/database/categoria.prisma.repository";
 import { PrismaClient } from "@prisma/client";
 import { DomainException } from "@shared/domain/domain.exception";
 
-const prisma = new PrismaClient();
+const prisma = new PrismaClient({
+    log:['query', 'info'],
+    errorFormat: 'pretty'
+});
 
 async function main() {
 
-    ///////////////////////
-    ////criar categoria////
-    ///////////////////////
-
-   /* let categoria: Categoria;
-    categoria = Categoria.criar({nome:'mesa'})
-
-    /////////////////////////////////
-    ///persistir Categoria no Banco//
-    /////////////////////////////////
-
-    await prisma.categoria.create({
-        data: {
-            id: categoria.id,
-            nome: categoria.nome
+    prisma.$connect().then(
+        async () => {
+            console.log('Postgres Conectado!');
         }
-    }); */
+    );
 
+    const categoriaRepo = new CategoriaPrismaRepository(prisma);
 
-    /////////////////////////////////
-    // atualizar categoria no Banco//
-    /////////////////////////////////
-
-    const categoriaAtualizada = await prisma.categoria.update({
-        where: {id:"1697d7a9-54c8-475a-81c2-d98733d733b7"},
-        data: {nome: 'Banho'},
-    })
-
-
-
-
-    //////////////////////
-    //listar categoria////
-    /////////////////////
-    const listarCategoria = await prisma.categoria.findMany();
-    console.log(listarCategoria);
 }
 
 main()
---
+++ src/shared/domain/repository.interface.ts
@@ -0,0 +1,16 @@
+
+interface IQuery<T> {
+    recuperarPorUuid(uuid: string): Promise<T | null>;
+    recuperartodos(): Promise<Array<T>>;
+    existe(uuid: string): Promise<Boolean>;
+}
+
+interface ICommand<T> {
+    inserir(entity: T): Promise<T>;
+    atualizar(uuid: string, entity: Partial<T>): Promise<Boolean>;
+    deletar(uuid: string): Promise<Boolean>;
+}
+
+interface IRepository<T> extends IQuery<T>, ICommand<T> {};
+
+export { IRepository};
